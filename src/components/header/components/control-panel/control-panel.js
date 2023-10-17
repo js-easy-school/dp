be1e044959d560f1abfa56935e45f21f2595d559
js-easy-school <supergeroy11@gmail.com>
2023-10-17 23:01:28 +0300

    Реализовать страницу Авторизации
    modified:   src/components/header/components/control-panel/control-panel.js
    modified:   src/components/icon/icon.js
    modified:   src/pages/authorization/authorization.js
    new file:   src/reducers/app-reducer.js
    modified:   src/reducers/index.js
    modified:   src/store.js

--- src/components/header/components/control-panel/control-panel.js
+++ src/components/header/components/control-panel/control-panel.js
@@ -3,18 +3,28 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Button, Icon } from '../../../../components';
 import styled from 'styled-components';
 import { ROLE } from './../../../../constants';
-import { selectUserRole, selectUserLogin, selectUserSession } from '../../../../selectors';
+import {
+	selectUserRole,
+	selectUserLogin,
+	selectUserSession,
+} from '../../../../selectors';
 import { logout } from '../../../../actions/logout';
 
 const RightAligned = styled.div`
 	display: flex;
 	justify-content: flex-end;
+	align-items: center;
 `;
 
 const StyledIcon = styled.div`
 	&:hover {
 		cursor: pointer;
 	}
+`;
+
+const UserName = styled.div`
+	font-size: 18px;
+	font-weight: bold;
 `;
 
 const ControlPanelContainer = ({ className }) => {
@@ -27,18 +37,22 @@ const ControlPanelContainer = ({ className }) => {
 	return (
 		<div className={className}>
 			<RightAligned>
-				<Button>
-					{roleId === ROLE.GUEST ? (
+				{roleId === ROLE.GUEST ? (
+					<Button>
 						<Link to="/login">Войти</Link>
-					) : (
-						<>
-							<div>{login}</div>
-							<StyledIcon onClick={() => dispatch(logout(session))}>
-								<Icon id="fa-sign-out" margin="10px 0 0 0" />
-							</StyledIcon>
-						</>
-					)}
-				</Button>
+					</Button>
+				) : (
+					<>
+						<UserName>{login}</UserName>
+						<StyledIcon>
+							<Icon
+								id="fa-sign-out"
+								margin="0 0 0 10px"
+								onClick={() => dispatch(logout(session))}
+							/>
+						</StyledIcon>
+					</>
+				)}
 			</RightAligned>
 			<RightAligned>
 				<StyledIcon onClick={() => navigate(-1)}>
